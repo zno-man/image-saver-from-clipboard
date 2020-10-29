3118133ef2de776e2ea8afed1f4b76da15ec071f
zno-man <59720725+zno-man@users.noreply.github.com>
2020-10-29 18:52:32 +0530

    Add files via upload

---
+++ formal image saver from clipboard.py
@@ -0,0 +1,35 @@
+from PIL import ImageGrab
+from win32 import win32clipboard
+import time
+import msvcrt
+name=time.time()
+
+name=(str(name))
+
+
+im = ImageGrab.grabclipboard()
+print('do you wish to save all images from the clip board as png files :')
+print('\npress 1+enter for yes')
+print('press enter to continue without saving \n \t\t___',end="\b\b")
+n=input()
+
+
+if n==1:
+    n=True
+try:
+    im.show()
+    
+except:
+    win32clipboard.OpenClipboard()
+    data = win32clipboard.GetClipboardData()
+    print("this text was previously copied to the clipboard not an image :\n---------------------------------------------------------------------------------------- \n",data)
+    print("\n----------------------------------------------------------------------------------------")
+    win32clipboard.CloseClipboard()
+else:
+
+    print("\n\n the image has been saved with the name {}.png ".format(name))
+    im.save(name+'.png','PNG')
+    im.close()
+
+print("\n\n  press enter to exit...")
+msvcrt.getch()
